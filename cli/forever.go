9dc3ecbdbc46132999db51bf9241cd04f96c140e
Howard Yeh <howard@metacircus.com>
2016-12-30 11:45:20 +0800

    shortcut to drop restart.txt

--- cli/forever.go
+++ cli/forever.go
@@ -7,16 +7,21 @@ import (
 )
 
 var (
-	minUptime     = kingpin.Flag("minUptime", "Minimum uptime for a script to not be considered 'spinning'").Duration()
-	spinSleepTime = kingpin.Flag("spinSleepTime", "Interval between restarts if a child is spinning").Duration()
-	restartFile   = kingpin.Flag("restartFile", "Drop a restart file. Touch the file to restart child.").String()
-	cmd           = kingpin.Arg("cmd", "command to run").Required().String()
-	args          = kingpin.Arg("args", "arguments").Strings()
+	minUptime       = kingpin.Flag("minUptime", "Minimum uptime for a script to not be considered 'spinning'").Duration()
+	spinSleepTime   = kingpin.Flag("spinSleepTime", "Interval between restarts if a child is spinning").Duration()
+	dropRestartFile = kingpin.Flag("dropRestartFile", "Touch the restart.txt to restart child.").Short('r').Bool()
+	restartFile     = kingpin.Flag("restartFile", "Touch the file to restart child.").String()
+	cmd             = kingpin.Arg("cmd", "command to run").Required().String()
+	args            = kingpin.Arg("args", "arguments").Strings()
 )
 
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
+
+	if *dropRestartFile && *restartFile == "" {
+		*restartFile = "restart.txt"
+	}
 	// kingpin.
 	// log.Println(minUptime, spinSleepTime, *cmd, *args)
 	forever.Start(*cmd, *args, &forever.Options{
